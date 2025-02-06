a2d8fdf9cbe8836934eaca32a225d8e34305da6d
guilhermesvm <guilhermesvmachado@gmail.com>
2025-02-05 21:28:56 -0300

    init: final ByteBank-FileIO version

---
+++ ByteBank-FileIO/6_ConsoleStream.cs
@@ -0,0 +1,21 @@
+﻿using ByteBank_FileIO.Models;
+
+partial class Program
+{
+    public static void ConsoleStream()
+    {
+        using (var stream = Console.OpenStandardInput())
+        using(var fs = new FileStream("console.txt", FileMode.Create))
+        {
+            byte[] buffer = new byte[1024];
+            while (true)
+            {
+                
+                var readBytes = stream.Read(buffer, 0, buffer.Length);
+                fs.Write(buffer, 0, readBytes);
+                fs.Flush();
+                Console.WriteLine($"Read bytes in console: {readBytes}");
+            }
+        }
+    }
+}
--- ByteBank-FileIO/Program.cs
+++ ByteBank-FileIO/Program.cs
@@ -8,7 +8,8 @@ partial class Program
         //HandlingStreamReader();
         //CreatingFile();
         //HandlingStreamWriter();
-        BinaryWriter();
-        BinaryStream();
+        //BinaryWriter();
+        //BinaryStream();
+        ConsoleStream();
     }
 }
